fc656f64da173eb788e0e51b85a93698584be51a
Aleksandr Zajac <mr.goodalex@gmail.com>
2020-09-22 23:25:17 +0300

    fix: esLinter config

--- js/stat.js
+++ js/stat.js
@@ -26,24 +26,24 @@ const renderCloud = (ctx, x, y, color) => {
 
 window.renderStatistics = (ctx, players, times) => {
   renderCloud(
-    ctx,
-    CLOUD_X + CLOUD_GAP,
-    CLOUD_Y + CLOUD_GAP,
-    'rgba(0, 0, 0, 0.7)');
+      ctx,
+      CLOUD_X + CLOUD_GAP,
+      CLOUD_Y + CLOUD_GAP,
+      `rgba(0, 0, 0, 0.7)`);
   renderCloud(
-    ctx,
-    CLOUD_X,
-    CLOUD_Y,
-    '#fff'
+      ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      `#fff`
   );
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = `#000`;
 
-  ctx.fillText('Ура вы победили! ', CONGRATULATIONS_TEXT_X, CONGRATULATIONS_TEXT_Y);
-  ctx.fillText('Список результатов:', LIST_OF_REZULTS_X, LIST_OF_REZULTS_Y);
+  ctx.fillText(`Ура вы победили! `, CONGRATULATIONS_TEXT_X, CONGRATULATIONS_TEXT_Y);
+  ctx.fillText(`Список результатов:`, LIST_OF_REZULTS_X, LIST_OF_REZULTS_Y);
 
   const getMaxElement = (arr) => {
-    var maxElement = arr[0];
+    let maxElement = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
       if (arr[i] > maxElement) {
@@ -56,45 +56,28 @@ window.renderStatistics = (ctx, players, times) => {
 
   for (let i = 0; i < players.length; i++) {
 
-    ctx.font = "16px PT Mono";
-    ctx.fillStyle = 'black';
+    ctx.font = `16px PT Mono`;
+    ctx.fillStyle = `black`;
 
     ctx.fillText(
-      players[i],
-      FIRST_PLAYER_NAME_X + FONT_GAP * i,
-      FIRST_PLAYER_NAME_Y
+        players[i],
+        FIRST_PLAYER_NAME_X + FONT_GAP * i,
+        FIRST_PLAYER_NAME_Y
     );
 
     ctx.fillText(
-      Math.round(times[i]),
-      FIRST_PLAYER_NAME_X + FONT_GAP * i,
-      FIRST_PLAYER_BAR_Y - SCORE_GAP + FIRST_PLAYER_BAR_HEIGHT - FIRST_PLAYER_BAR_HEIGHT / getMaxElement(times) * Math.round(times[i]),
+        Math.round(times[i]),
+        FIRST_PLAYER_NAME_X + FONT_GAP * i,
+        FIRST_PLAYER_BAR_Y - SCORE_GAP + FIRST_PLAYER_BAR_HEIGHT - FIRST_PLAYER_BAR_HEIGHT / getMaxElement(times) * Math.round(times[i])
     );
 
-    players[i] == 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'hsl(241, 100%,' + Math.floor(Math.random() * 99 + 1) + '%)';
+    ctx.fillStyle = players[i] === `Вы` ? `rgba(255, 0, 0, 1)` : `hsl(241, 100%,` + Math.floor(Math.random() * 99 + 1) + `%)`;
 
     ctx.fillRect(
-      FIRST_PLAYER_BAR_X + BAR_WIDTH * i,
-      FIRST_PLAYER_BAR_Y + FIRST_PLAYER_BAR_HEIGHT - FIRST_PLAYER_BAR_HEIGHT / getMaxElement(times) * Math.round(times[i]),
-      FIRST_PLAYER_BAR_WIDTH,
-      FIRST_PLAYER_BAR_HEIGHT / getMaxElement(times) * Math.round(times[i])
+        FIRST_PLAYER_BAR_X + BAR_WIDTH * i,
+        FIRST_PLAYER_BAR_Y + FIRST_PLAYER_BAR_HEIGHT - FIRST_PLAYER_BAR_HEIGHT / getMaxElement(times) * Math.round(times[i]),
+        FIRST_PLAYER_BAR_WIDTH,
+        FIRST_PLAYER_BAR_HEIGHT / getMaxElement(times) * Math.round(times[i])
     );
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
